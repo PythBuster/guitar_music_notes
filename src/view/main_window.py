a8648a9a147a38e40273bb2e7aa318e5167258c9
PythBuster <pythbuster@gmail.com>
2023-09-10 01:48:35 +0200

    feat: save and load settings, so all configurations in main window will be persistent + set app version to v2.0.0

--- src/view/main_window.py
+++ src/view/main_window.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+from PySide6.QtCore import QSettings, QByteArray
 from PySide6.QtWidgets import QCheckBox, QMainWindow, QMessageBox
 
 from src.config import IMAGES_PATH, SOUNDS_DIR
@@ -9,14 +10,16 @@ from src.view.viewer import Viewer
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self, app_data: dict[str, Any] | None = None):
-        if app_data is None:
-            app_data = {}
+    def __init__(self, app_data: dict[str, Any]):
+        self.app_data = app_data
 
         super().__init__()
         self.setupUi(self)
 
         self.setWindowTitle(f"{app_data['name']} v{app_data['version']}")
+
+        self.viewer = None
+        self.setFixedSize(self.sizeHint())
 
         # connections
         self.radioButton_timer.clicked.connect(
@@ -31,8 +34,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         )
         self.actionAbout_Guitar_Music_Notes.triggered.connect(self.about_app)
 
-        self.viewer = None
-        self.setFixedSize(self.sizeHint())
+        self.read_settings()
 
     def about_app(self):
         about = QMessageBox(self)
@@ -73,3 +75,57 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         }
 
         return selected_notes
+
+    def write_settings(self) -> None:
+        settings = QSettings(self.app_data['name'])
+
+        settings.beginGroup("MainWindow")
+
+        settings.setValue("geometry", self.saveGeometry())
+
+        radioButton_timer_is_checked = self.radioButton_timer.isChecked()
+        settings.setValue("radioButton_timer", radioButton_timer_is_checked)
+        settings.setValue("lineEdit_timer_seconds", self.lineEdit_timer_seconds.text())
+
+        selected_note_types = self.collect_selected_notes()
+        settings.setValue("selected_note_types", selected_note_types)
+
+        settings.endGroup()
+
+    def read_settings(self) -> None:
+        settings = QSettings(self.app_data['name'])
+
+        settings.beginGroup("MainWindow")
+
+        geometry = settings.value("geometry", QByteArray())
+
+        if not geometry.isEmpty():
+            self.restoreGeometry(geometry)
+
+        radioButton_timer_is_checked = settings.value("radioButton_timer", False, type=bool)
+        self.radioButton_timer.setChecked(radioButton_timer_is_checked)
+        self.radioButton_manually.setChecked(not radioButton_timer_is_checked)
+        self.groupBox_timer_settings.setVisible(radioButton_timer_is_checked)
+
+        lineEdit_timer_seconds = settings.value("lineEdit_timer_seconds", "4")
+        self.lineEdit_timer_seconds.setText(lineEdit_timer_seconds)
+
+        selected_note_types = settings.value("selected_note_types", set())
+        self.select_notes(selected_note_types=selected_note_types)
+
+        settings.endGroup()
+
+    def select_notes(self, selected_note_types: set[NoteType]) -> None:
+        checkboxes = [
+            child
+            for child in self.groupBox_select_notes.children()
+            if isinstance(child, QCheckBox)
+        ]
+
+        for checkbox in checkboxes:
+            checkbox.setChecked(NoteType(checkbox.objectName().replace("note_", "").replace("_", "'")) in selected_note_types)
+
+    def closeEvent(self, event):
+        self.write_settings()
+        self.viewer.close()
+        event.accept()
